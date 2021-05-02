2c0c7b8c8867693e7dfb8e4c08e37a23b26bb1e7
ryo-tokuda <tokuda19920105@gmail.com>
2021-05-02 17:04:24 +0900

    API取得

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
+import React, {useState,useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [images,setImages] = useState([]);
+  const [text,setText] = useState('');
+  const [query,setQuery] = useState('apple');
+
+  useEffect(() => {
+    console.log('useEffectが走りました。')
+    fetch(`https://api.unsplash.com/search/photos?query=${query}&client_id=${process.env.REACT_APP_CLIENT_ID}`)
+    .then(response => response.json())
+    .then(data =>{ 
+      console.log(data)
+      setImages(data.results)
+    })
+  },[query])
+ 
+  const onSubmit = (e) =>{
+    e.preventDefault();
+    setQuery(text);
+    setText('');
+    console.log("onSubmitが呼ばれました")
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className="main">
+      <form onSubmit={onSubmit}> 
+        <input type="text"
+        onChange= {e =>setText(e.target.value)}
+        value = {text}
+        />
+        <button type="submit">
+          Search
+        </button>
+      </form>
+     </div>
+     <div className="container">
+      {
+      images.map(image =>(
+        <div key= {image.id} className="card">
+          <img src={image.urls.regular} className="card-img" alt=""  />
+          {/*images.urls.regularはコンソールで確認する  */}
+          <div className="card-content">
+            <h1 className="card-title">
+                {image.alt_description}
+                {/* image.alt_descriptionはコンソールで確認する */}
+            </h1>
+          </div>
+        </div>
+      ))
+      }
+     </div>
     </div>
   );
 }
